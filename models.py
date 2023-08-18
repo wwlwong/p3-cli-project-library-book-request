13f8bdecec2c829058a26108dee2b808fb187ada
William Wong <william.wai.lim.wong@gmail.com>
2023-08-18 13:06:31 -0400

    creates schema for patron

--- models.py
+++ models.py
@@ -1,6 +1,15 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
-    pass
+    
+    __tablename__ = 'patron'
+    
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    phone = Column(Integer)
+    card_number = Column(Integer)
+    home_branch = Column(String)
